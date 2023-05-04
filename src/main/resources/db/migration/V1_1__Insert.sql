a968ec58cc335105991473aa96cbc037f55e5c7a
Sushant Sinha <sushant.sinha@medvanture.com>
2023-05-04 10:55:19 +0530

    flyway db migration implementation

---
+++ src/main/resources/db/migration/V1_1__Insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO `validuserschema`.`user`
+(`user_id`,
+ `age`,
+ `email`,
+ `gender`,
+ `mobile`,
+ `name`,
+ `nationality`)
+VALUES
+    (50,
+     21,
+     "abc@gmail.com",
+     "Male",
+     8888888888,
+     "Danny",
+     "Indian");
